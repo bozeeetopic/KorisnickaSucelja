29cab39135d0eedc96ac0c44b740b91e5a3edf86
bozeeetopic <btopic00@fesb.hr>
2021-10-27 12:18:17 +0200

    Tailwind Labovi Finalno
    commit nakon vjezbi

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+  './components/**/*.{js,ts,jsx,tsx}',
+  './modules/**/*.{js,ts,jsx,tsx}',
+  './pages/**/*.{js,ts,jsx,tsx}'
+],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
